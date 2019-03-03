f82d95c56438654bfe7740e2726cf7716ce1e8e6
spitutz <45382026+spitutz@users.noreply.github.com>
2019-03-04 01:54:52 +0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,61 +1,52 @@
-function Set_Cookie(name, value, expires, path, domain, secure) {
-var today = new Date();
-today.setTime(today.getTime());
-var expires_date = new Date(today.getTime() + (expires));
+//Pop-under window- By JavaScript Kit
+//Credit notice must stay intact for use
+//Visit http://javascriptkit.com for this script
 
-document.cookie = name + "=" + escape(value) +
-((expires) ? ";expires=" + expires_date.toGMTString() : "") +
-((path) ? ";path=" + path : "") +
-((domain) ? ";domain=" + domain : "") +
-((secure) ? ";secure" : "");
+//specify page to pop-under
+var popunder="http://ibiet.blogspot.com/"
+
+//specify popunder window features
+//set 1 to enable a particular feature, 0 to disable
+var winfeatures="width=800,height=510,scrollbars=1,resizable=1,toolbar=1,location=1,menubar=1,status=1,directories=0"
+
+//Pop-under only once per browser session? (0=no, 1=yes)
+//Specifying 0 will cause popunder to load every time page is loaded
+var once_per_session=1
+
+///No editing beyond here required/////
+
+function get_cookie(Name) {
+  var search = Name + "="
+  var returnvalue = "";
+  if (document.cookie.length > 0) {
+    offset = document.cookie.indexOf(search)
+    if (offset != -1) { // if cookie exists
+      offset += search.length
+      // set index of beginning of value
+      end = document.cookie.indexOf(";", offset);
+      // set index of end of cookie value
+      if (end == -1)
+         end = document.cookie.length;
+      returnvalue=unescape(document.cookie.substring(offset, end))
+      }
+   }
+  return returnvalue;
 }
 
-function Get_Cookie(name) {
-
-var start = document.cookie.indexOf(name + "=");
-var len = start + name.length + 1;
-if ((!start) &&
-(name != document.cookie.substring(0, name.length))) {
-return null;
-}
-if (start == -1) return null;
-var end = document.cookie.indexOf(";", len);
-if (end == -1) end = document.cookie.length;
-return unescape(document.cookie.substring(len, end));
-}
-
-function Delete_Cookie(name, path, domain) {
-if (Get_Cookie(name)) document.cookie = name + "=" +
-((path) ? ";path=" + path : "") +
-((domain) ? ";domain=" + domain : "") +
-";expires=Mon, 11-November-1989 00:00:01 GMT";
-}
-
-function popunder() {
-
-if (Get_Cookie('cucre') == null) {
-Set_Cookie('cucre', 'cucre Popunder', '1', '/', '', '');
-var url = "https://nuansahati90.blogspot.com/";
-pop = window.open(url, 'windowcucre');
-pop.blur();
-
-window.focus();
-}}
-
-function addEvent(obj, eventName, func) {
-if (obj.attachEvent) {
-obj.attachEvent("on" + eventName, func);
-}
-else if (obj.addEventListener) {
-obj.addEventListener(eventName, func, true);
-}
-else {
-obj["on" + eventName] = func;
+function loadornot(){
+if (get_cookie('popunder')==''){
+loadpopunder()
+document.cookie="popunder=yes"
 }
 }
 
-addEvent(window, "load", function (e) {
-addEvent(document.body, "click", function (e) {
-popunder();
-});
-});
+function loadpopunder(){
+win2=window.open(popunder,"",winfeatures)
+win2.blur()
+window.focus()
+}
+
+if (once_per_session==0)
+loadpopunder()
+else
+loadornot()
