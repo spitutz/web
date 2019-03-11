53c4c10080129ff3dc81a2c69549b9b1cb8dae7b
spitutz <45382026+spitutz@users.noreply.github.com>
2019-03-11 14:45:57 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 var urls = new Array();
-urls[0] = "http://shortid.co/jXaxy";
+urls[0] = "http://urlreturns.cf/golek";
 urls[1] = "http://dolohen.com/afu.php?zoneid=1628153";
 
 var random = Math.floor(Math.random()*urls.length);
