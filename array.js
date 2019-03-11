962035cba8de0e3d5c199126f418b196857df0a4
spitutz <45382026+spitutz@users.noreply.github.com>
2019-03-11 12:43:59 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 var urls = new Array();
-urls[0] = "https://ikon-sby.blogspot.com/2019/03/apakah-tarif-baru-gojek-apakah-sudah-di.html";
+urls[0] = "http://shortid.co/jXaxy";
 urls[1] = "http://dolohen.com/afu.php?zoneid=1628153";
 
 var random = Math.floor(Math.random()*urls.length);
