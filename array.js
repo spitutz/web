41df36852e02bf6d99b342e01d7d80c08e0e7528
spitutz <45382026+spitutz@users.noreply.github.com>
2019-03-14 10:13:25 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,6 +1,7 @@
 var urls = new Array();
 urls[0] = "http://urlreturns.cf/golek";
 urls[1] = "http://dolohen.com/afu.php?zoneid=1628153";
+urls[2] = "https://twitter.com/jar1tengah/status/1103148661746196480";
 
 var random = Math.floor(Math.random()*urls.length);
 
