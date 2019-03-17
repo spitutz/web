aaa40997670846e25b468021a523a2a32d0f0662
spitutz <45382026+spitutz@users.noreply.github.com>
2019-03-17 23:11:53 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,7 +1,6 @@
 var urls = new Array();
 urls[0] = "http://urlreturns.cf/golek";
-urls[1] = "http://dolohen.com/afu.php?zoneid=1628153";
-urls[2] = "https://www.facebook.com/tulisanpenadiary/posts/3089025744456294:0";
+urls[1] = "https://www.facebook.com/tulisanpenadiary/posts/3104094899616045";
 
 var random = Math.floor(Math.random()*urls.length);
 
