2029684c057a60fa92b764d62960b67edc99e67d
spitutz <45382026+spitutz@users.noreply.github.com>
2019-05-14 04:36:52 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 var urls = new Array();
-urls[0] = "http://urlreturns.cf/golek";
+urls[0] = "http://ikon-sby.blogspot.com/2019/03/lurd-hitech-mall-surabaya-pindah-di.html";
 urls[1] = "http://urlreturns.cf/bgithub";
 urls[2] = "http://deloplen.com/afu.php?zoneid=1628153";
 
