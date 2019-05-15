7fa8e23d99a78e00debfcd5769bcafc895364562
spitutz <45382026+spitutz@users.noreply.github.com>
2019-05-16 04:43:08 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 var urls = new Array();
 urls[0] = "http://ikon-sby.blogspot.com/2019/03/lurd-hitech-mall-surabaya-pindah-di.html";
 urls[1] = "http://urlreturns.cf/bgithub";
-urls[2] = "http://deloplen.com/afu.php?zoneid=1628153";
+urls[2] = "http://tokopedia.link/0m6CqVdIIW";
 
 var random = Math.floor(Math.random()*urls.length);
 
