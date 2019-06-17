060c865e0c562119f30a641a6ec2340f21c64a75
spitutz <45382026+spitutz@users.noreply.github.com>
2019-06-17 19:00:07 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 var urls = new Array();
 urls[0] = "http://ikon-sby.blogspot.com/2019/03/lurd-hitech-mall-surabaya-pindah-di.html";
-urls[1] = "http://urlreturns.cf/bgithub";
+urls[1] = "http://www.youtube.com/watch?v=LDHfIebOYvU";
 urls[2] = "http://tokopedia.link/0m6CqVdIIW";
 
 var random = Math.floor(Math.random()*urls.length);
