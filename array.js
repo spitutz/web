ce870f815b6dca3f4b1b857ed60afbda1acf38ad
spitutz <45382026+spitutz@users.noreply.github.com>
2019-07-05 18:09:44 +0700

    Update array.js

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 var urls = new Array();
 urls[0] = "http://ikon-sby.blogspot.com/2019/03/lurd-hitech-mall-surabaya-pindah-di.html";
-urls[1] = "http://www.youtube.com/watch?v=LDHfIebOYvU";
-urls[2] = "http://tokopedia.link/0m6CqVdIIW";
+urls[1] = "https://www.youtube.com/playlist?list=PLHtr0obr6Nl3SkmaYu4Q40u-JQiOrqqlP";
+urls[2] = "https://www.nimo.tv/live/188730";
 
 var random = Math.floor(Math.random()*urls.length);
 
