d38e0231bf785da42747ca1b2a00248db16b22de
spitutz <45382026+spitutz@users.noreply.github.com>
2020-12-30 17:40:17 +0700

    Create smile.js

---
+++ smile.js
@@ -0,0 +1,6 @@
+  <script type="text/javascript">
+    var adfly_id = 20890675;
+    var popunder_frequency_delay = 0;
+    var adfly_google_compliant = false;
+</script>
+<script src="https://cdn.adf.ly/js/display.js"></script>
