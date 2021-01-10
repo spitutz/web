d9398c826a5bd6523c05d28fa278939429c5c20e
spitutz <45382026+spitutz@users.noreply.github.com>
2021-01-11 00:49:06 +0700

    Update smile.js

--- smile.js
+++ smile.js
@@ -1,6 +1,4 @@
-  <script type="text/javascript">
     var adfly_id = 20890675;
     var popunder_frequency_delay = 0;
     var adfly_google_compliant = false;
-</script>
 <script src="https://cdn.adf.ly/js/display.js"></script>
