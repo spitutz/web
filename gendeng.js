9ba03a6cebd8f194f5df851567399a151809d5cc
spitutz <45382026+spitutz@users.noreply.github.com>
2021-02-23 20:19:59 +0700

    Create gendeng.js

---
+++ gendeng.js
@@ -0,0 +1,16 @@
+
+function popup() {
+
+var url = "http://twitter.com/matamelototid";
+
+window.open(url);
+
+popWin = window.open(page,'popWin');
+
+popWin.blur();
+
+window.focus();
+
+}
+
+popup();
