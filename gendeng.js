706fc9a2d52da602aed508634934b17d2b867cdb
spitutz <45382026+spitutz@users.noreply.github.com>
2021-09-19 05:05:32 +0700

    Update gendeng.js

--- gendeng.js
+++ gendeng.js
@@ -1,5 +1,5 @@
 var target = [
-"https://www.youtube.com/watch?v=_eyZXU2WoTY&t=200s",
+"https://lynk.id/grubviral",
 "https://www.highperformancecpm.com/uw67djb6v?key=365a89d62f8404c34146b5d5cf2a699e"
 ];
 var maxclick=1;
