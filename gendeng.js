41c1768834ecd5fd2c2048c60a745ee028afdedd
spitutz <45382026+spitutz@users.noreply.github.com>
2021-10-23 23:34:17 +0700

    Update gendeng.js

--- gendeng.js
+++ gendeng.js
@@ -1,5 +1,5 @@
 var target = [
-"https://www.youtube.com/watch?v=w5IHL2BLRYI",
+"https://cucimata.my.id/go/IC9lh",
 "https://www.highperformancecpm.com/uw67djb6v?key=365a89d62f8404c34146b5d5cf2a699e"
 ];
 var maxclick=3;
