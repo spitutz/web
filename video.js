665d5445bd97122b656bef1f85c12d33ae0bb9c6
spitutz <45382026+spitutz@users.noreply.github.com>
2021-10-27 08:09:34 +0300

    Update video.js

--- video.js
+++ video.js
@@ -1,5 +1,5 @@
 var target = [
-"https://cucimata.my.id/go/IC9lh",
+"https://cucimata.my.id",
 "https://www.highperformancecpm.com/uw67djb6v?key=365a89d62f8404c34146b5d5cf2a699e"
 ];
 var maxclick=2;
