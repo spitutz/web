fe1081551a17734b9b96a4cc248e30b14b7046cb
spitutz <45382026+spitutz@users.noreply.github.com>
2021-12-12 17:03:57 +0700

    Create ini.js

---
+++ ini.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+var count = 10;
+var redirect = new Array();
+redirect[0] = "https://pin.it/3MkbAcm";
+redirect[1] = "https://pin.it/3MkbAcm";
+
+var random = Math.floor(Math.random()*redirect.length);
+
+function countDown(){
+    var timer = document.getElementById("timer");
+    if(count > 0){
+        count--;
+        timer.innerHTML = "Mohon Tunggu "+count+" Detik.";
+        setTimeout("countDown()", 1000);
+    }else{
+        window.location.href = redirect[random];
+    }
+}
+</script>
