bc8e6cf462b0cebfd0639846f9c3e53f1bee2af7
spitutz <45382026+spitutz@users.noreply.github.com>
2021-12-12 17:20:47 +0700

    Update ini.js

--- ini.js
+++ ini.js
@@ -1,17 +1,7 @@
-var count = 10;
-var redirect = new Array();
-redirect[0] = "https://pin.it/3MkbAcm";
-redirect[1] = "https://pin.it/3MkbAcm";
+var urls = new Array();
+urls[0] = "https://pin.it/3MkbAcm";
+urls[1] = "https://pin.it/3MkbAcmt";
 
-var random = Math.floor(Math.random()*redirect.length);
+var random = Math.floor(Math.random()*urls.length);
 
-function countDown(){
-    var timer = document.getElementById("timer");
-    if(count > 0){
-        count--;
-        timer.innerHTML = "Mohon Tunggu "+count+" Detik.";
-        setTimeout("countDown()", 1000);
-    }else{
-        window.location.href = redirect[random];
-    }
-}
+window.location = urls[random];
