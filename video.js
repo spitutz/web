b1a53ceb74891e429064bc942996a7c569869571
spitutz <45382026+spitutz@users.noreply.github.com>
2023-01-05 12:27:44 +0700

    Update video.js

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 var target = [
 "https://twitter.com/noke_sp/status/1606018305088946176?s=20&t=lj2l_TQVaDHD8zme8Y9pwg",
-"https://tokopedia.link/cMPBfayqcwb"
+"https://tokopedia.link/iZqUIcJHkwb"
 ];
 var maxclick=1;
 function tampilkanUrl(){
